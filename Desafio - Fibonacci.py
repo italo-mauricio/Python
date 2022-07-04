49e722c6f3a3d21d3e31e0fdd3dd3d8d00d91efd
Italo Maurício <105960758+italomauricio1@users.noreply.github.com>
2022-07-04 11:46:52 -0300

    Fibonacci

---
+++ Desafio - Fibonacci.py
@@ -0,0 +1,19 @@
+print("Vamos criar uma sequência de Fibonacci !")
+
+n = int(input("Digite a quantidade de termos que você quer mostrar: "))
+t1 = 0
+t2 = 1
+
+print("=="*30)
+print(f'{t1} -> {t2}', end='')
+cont = 3
+
+while cont <= n:
+    t3 = t1 + t2
+    print(f' -> {t3}', end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+
+print(' -> FIM')
+print('A soma dos termos apresentados é {}'.format( (t1 + t2 + t3) - 1))
