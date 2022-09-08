411016efafe21257e9524fb406c7b4d84cedaa6b
Italo Mauricio <105960758+italomauricio1@users.noreply.github.com>
2022-09-08 15:47:52 -0300

    trabalhando com analise de dados

---
+++ Desafios/analise-de-dados-numa-tupla.py
@@ -0,0 +1,9 @@
+num1 = input("Digite um número: ")
+num2 = input("Digite um número: ")
+num3 = input("Digite um número: ")
+num4 = input("Digite um número: ")
+
+numeros = (num1, num2, num3, num4)
+
+for i in numeros:
+    print(f"Os números foram {i}")
