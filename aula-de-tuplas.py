0f607e7d1adad0afd1830e27c034e968173c4de5
Italo Mauricio <105960758+italomauricio1@users.noreply.github.com>
2022-09-07 23:26:41 -0300

    update

--- aula-de-tuplas.py
+++ aula-de-tuplas.py
@@ -1,6 +1,10 @@
 lanche = ('hamburguer', 'pizza', 'pudim')
 # Tuplas são imutáveis
 # exemplo {Lanche[1] = 'refrigerante' '} daria erro de syntax, pois tuplas são imutáveis
-for comida in lanche:
-    print(f'eu vou comer {comida}')
+#for comida in lanche:
+    #print(f'eu vou comer {comida}')
+#print('comi muito')
+
+for cont in range(0, len(lanche)):
+    print(lanche[cont])
 print('comi muito')
