bffc778e6a9424c76376fe7e90521a28a2956c35
Italo Mauricio <105960758+italomauricio1@users.noreply.github.com>
2022-09-08 10:26:25 -0300

    update série c

--- lista-de-times.py
+++ lista-de-times.py
@@ -26,6 +26,7 @@ def main():
         elif opção == '3':
             break
         elif opção == '4':
+            print("Obrigado pela preferência.")
             break
         else:
             print('Opção inválida!')
@@ -91,6 +92,31 @@ def timesb():
 
 
 
+
+def timesc():
+    os.system("cls")
+    while True:
+        times = ('Mirassol', 'Volta Redonda', 'Aparecidense','ABC',
+        'Figueirense', 'Vitória', 'Paysandu', 'São José-RS', 'Remo',
+        'Manaus', 'Confiança', 'Floresta', 'Altos', 'Ypiranga-RS',
+        'Ferroviário', 'Campinense', 'Brasil de Pelotas', 'Atlético-CE', 'Ponte Preta-PB',
+        'Atlético-CE')
+
+        print(f'''Lista de todos os times da Série C do Brasileirão
+
+            Lista de times {times}
+
+            Os cinco primeiros {times[:5]}
+
+            Os quatro últimos são {times[-4:]}
+
+            Os times em ordem alfabética{sorted(times)}
+
+
+        ''')
+
+        cont = input("Aperte ENTER para retornar ao Menu...")
+        main()
 
 
 
