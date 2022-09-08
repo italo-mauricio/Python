a7d6f14cf44e74118bbd6a2b4b5fe238f808a8bb
Italo Mauricio <105960758+italomauricio1@users.noreply.github.com>
2022-09-08 15:58:46 -0300

    update

--- Desafios/analise-de-dados-numa-tupla.py
+++ Desafios/analise-de-dados-numa-tupla.py
@@ -7,10 +7,10 @@ num = (int(input("Digite um número: ")),
 print(f"Você digitou os valores {num}")
 print(f"O valor 9 apareceu {num.count(9)} vezes")
 if 3 in num:
-    print(f"O valor 3 apareceu na {num.index(3)+1} posição")
+    print(f"O valor 3 apareceu na {num.index(3)+1} posição") # evita que o código quebre quando o 3 não for digitado
 else:
     print("O número 3 não apareceu nenhuma vez")
 print('Os valores pares digitados foram', end='')
-for n in num:
+for n in num:  # irá verificar quantos números pares existem
     if n % 2 == 0:
         print(n, end='')
