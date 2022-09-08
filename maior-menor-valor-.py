88c1bb1496918fee6306f4f95be0b8de0501371f
Italo Mauricio <105960758+italomauricio1@users.noreply.github.com>
2022-09-08 15:43:09 -0300

    update

--- maior-menor-valor-.py
+++ maior-menor-valor-.py
@@ -9,5 +9,10 @@ for i in num:
     print(f"{i}", end='')
 
 
-print(f"O maior valor sorteado foi {max(num)}")
-print(f"O menor valor sorteado foi {min(num)}")
+print(f"\nO maior valor sorteado foi: {max(num)}")
+print(f"O menor valor sorteado foi: {min(num)}")
+
+if max(num) > 8:
+    print("O número sorteado foi acima do esperado")
+else:
+    print("Não atingiu")
