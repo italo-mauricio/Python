399ea931b071e052b25344881fb263ec898ea09f
Italo Mauricio <105960758+italomauricio1@users.noreply.github.com>
2022-09-07 23:30:24 -0300

    update

--- aula-de-tuplas.py
+++ aula-de-tuplas.py
@@ -5,6 +5,14 @@ lanche = ('hamburguer', 'pizza', 'pudim')
     #print(f'eu vou comer {comida}')
 #print('comi muito')
 
+#for cont in range(0, len(lanche)):
+    #print(lanche[cont])
+#print('comi muito')
+
 for cont in range(0, len(lanche)):
-    print(lanche[cont])
-print('comi muito')
+    print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+
+for pos, comida in enumerate(lanche): # o Enumerate retorna tanto o dado, quanto a posição
+    print(f'Eu vou comer {comida} na posição {pos}')
+
+print ('comi pra caramba')
