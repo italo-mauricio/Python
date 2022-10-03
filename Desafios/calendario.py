f7a1cb6d1ab95bdce37597cc47a132474d24227e
Italo Mauricio <105960758+italo-mauricio@users.noreply.github.com>
2022-10-03 11:39:57 -0300

    adicionando calendário completo

---
+++ Desafios/calendario.py
@@ -0,0 +1,25 @@
+import calendar
+from datetime import *
+
+yy = 2022
+mm = 1
+mm1 = 2
+mm3 = 3
+mm4 = 4
+mm5 = 5
+mm6 = 6
+mm7 = 7
+mm8 = 8
+mm9 = 9
+mm10 = 10
+mm11 = 11
+mm12 = 12
+
+
+
+print(calendar.month(yy,mm)), print(calendar.month(yy, mm1))
+print(calendar.month(yy,mm3)), print(calendar.month(yy, mm4))
+print(calendar.month(yy,mm5)), print(calendar.month(yy,mm6))
+print(calendar.month(yy,mm7)), print(calendar.month(yy,mm8))
+print(calendar.month(yy,mm9)), print(calendar.month(yy,mm10))
+print(calendar.month(yy,mm11)), print(calendar.month(yy,mm12))
