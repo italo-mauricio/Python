24e8eea127349529d49eef88e194e0ccc263d399
italo <italogithub@gmail.com>
2022-10-09 20:03:09 -0300

    update

--- experimentos/insta.py
+++ experimentos/insta.py
@@ -1,24 +1,70 @@
 from datetime import datetime
 import instaloader
 import pwinput
+from datetime import date
 
 
-L = instaloader.Instaloader()
-log = input('Digite o user do seu insta: ')
-senha = pwinput.pwinput("Digite a senha do seu insta: ")
+hora_atual = datetime.now()
+hora = hora_atual.strftime('%H:%M')
+data = date.today()
 
 
-L.login(log, senha)
+def instagram():
+    while True:
+        
+        print(f'''
+        | =========================================================== |
+        | ---------------- Verificador de Instagram ----------------- |
+        |                                                             |
+        | Requisitos: pip3 install instaloader                        |
+        |                                                             |
+        | A biblioteca retorna informações dos seus seguidores como,  |
+        | nome, id, bio, user etc. Lembrando, que é totalmente seguro |
+        | pois ele só retorna informações publicas.                   |
+        |                                                             |
+        | ----------------------------------------------------------- |
+        | =========================================================== |
+        |                since 2022, by italo-mauricio                |
+        | =========================================================== |
+            
+        ''')
+        
+        L = instaloader.Instaloader()
+        log = input('Digite o user do seu insta: ')
+        senha = pwinput.pwinput("Digite a senha do seu insta: ")
 
 
-followers = instaloader.Profile.from_username(L.context, log).get_followers()
-followees = instaloader.Profile.from_username(L.context, log).get_followees()
+        L.login(log, senha)
 
 
-print('Seguidores: ' + str(followers._data['count']))
-print('Seguindo: ' + str(followees._data['count']))
-print('\n')
-print('Lista e informações de seguidores: ')
-print('\n')
+        followers = instaloader.Profile.from_username(L.context, log).get_followers()
+        followees = instaloader.Profile.from_username(L.context, log).get_followees()
 
-print(followers._data['edges'])
+
+        print(f'''
+            
+            Data da verificação: {data}
+            Hora da verificação: {hora}
+            
+            Seguidores: ''' + str(followers._data['count']))
+        print('Seguindo: ' + str(followees._data['count']))
+        print('\n')
+        print('Lista e informações de seguidores: ')
+        print('\n')
+        print(followers._data['edges'])
+       
+        conti = input("Deseja verificar novamente: [Y,N] ").strip().lower()
+        
+        if conti == 'Y'.lower():
+            instagram()
+            break
+        if conti == 'N'.lower():
+            print("Obrigado!")
+            break
+        else:
+            print("Tente novamente!")
+            
+                
+                
+instagram()
+                
