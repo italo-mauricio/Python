517f3a02891073ebd7cb740b87b265572300297d
italo <italogithub@gmail.com>
2022-10-25 22:44:25 -0300

    testando bibliotecas de audio

--- experimentos/convert_txt_in_audio.py
+++ experimentos/convert_txt_in_audio.py
@@ -1,16 +1,24 @@
 from gtts import gTTS
 from playsound import playsound
 
+while True:
+    audio = 'audioteste.mp3'
+    language = 'pt-br'
+    usuario = input("Digite o que você quer falar: ")
 
-audio = 'audioteste.mp3'
-language = 'pt-br'
-usuario = input("Digite o que você quer falar: ")
-sp = gTTS(
-    
-    text = usuario,
-    lang = language
-    
-)
+    sp = gTTS(
+        
+        text = usuario,
+        lang = language
+    )
 
-sp.save(audio)
-playsound(audio)
+    sp.save(audio)
+    playsound(audio)
+ 
+    conti = input("Deseja falar novamente? [Y/N] ")
+    if conti == 'Y':
+        continue
+    elif conti == 'N':
+        print("Obrigado!")
+        break
+ 
