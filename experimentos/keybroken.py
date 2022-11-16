415cc8dabd1c2c1d9311e3a0ada4b62956aaa402
italo <italogithub@gmail.com>
2022-11-16 04:10:39 -0300

    update

--- experimentos/keybroken.py
+++ experimentos/keybroken.py
@@ -1,14 +1,46 @@
 import string
-from random import seed, random, choice
+import time
+import sys
+from datetime import timedelta
+from random import choice, seed, random
+from itertools import combinations_with_replacement
 
-
-values = string.ascii_letters  # todos os caracteres maiúsculos e minúsculos
-string.punctuation   # caracteres acentuados    
-string.digits  # números de 0 a 9 
-tamanho = 6
+'''values = string.ascii_letters  # todos os caracteres maiúsculos e minúsculos
+values += string.punctuation   # caracteres acentuados    
+values += string.digits  # números de 0 a 9 
+tamanho = 10
 senha = " "
 
 for i in range(tamanho):
     senha += choice(values)
 
-print(senha)
+print(senha)'''
+
+def main(args):
+
+    valores = string.ascii_letters
+    valores += string.digits
+    valores += string.punctuation
+
+    tamanho = 5
+
+    init_temp = time.time()
+    gerar_senhas(valores, tamanho)
+    init_fim = time.time()
+
+    temp = init_temp - init_fim
+
+
+    print(f"O tempo de execução foi de {temp:2f}s")
+
+
+
+
+def gerar_senhas(valores, tamanho):
+    comb = combinations_with_replacement(valores, tamanho)
+    print("Combinações:  " + str(len(list(comb))))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1: ]) 
+
