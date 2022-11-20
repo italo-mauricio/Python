277b971602ed8dc60b02de722289f5883fc768c6
italo <italogithub@gmail.com>
2022-11-19 22:11:55 -0300

    update

--- experimentos/country.py
+++ experimentos/country.py
@@ -1,18 +1,24 @@
 from countryinfo import CountryInfo
 
+while True:
+    countrys = CountryInfo(input("Digite o nome do país: "))
+    cont = 0
+    print(f'''
 
-countrys = CountryInfo(input("Digite o nome do país: "))
+        País: {countrys.name()}
+        Capital: {countrys.capital()}
+        Moedas: {countrys.currencies()}
+        Idiomas: {countrys.languages()}
+        Fronteiras: {countrys.borders()}
+        DDD: {countrys.calling_codes()}
+        População: {countrys.population()}
 
-print(f'''
+    ''')
 
-    País: {countrys.name()}
-    Capital: {countrys.capital()}
-    Moedas: {countrys.currencies()}
-    Idiomas: {countrys.languages()}
-    Fronteiras: {countrys.borders()}
-    DDD: {countrys.calling_codes()}
-    População: {countrys.population()}
-
-
-
-''')
+    conti = input("Deseja consultar novamente? [S/N] ")
+    if conti == 'S' or conti == 's':
+        cont += 1
+    if conti == 'N' or conti == 'n':
+        print("Thank You!")
+        break
+    
