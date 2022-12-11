c76984ab4b7e31393bb139189281e2e1b817e0cd
italo <italogithub@gmail.com>
2022-12-11 14:49:26 -0300

    adicionando aula de tuplas

--- Desafios/aula_de_tuplas.py
+++ Desafios/aula_de_tuplas.py
@@ -1,5 +1,33 @@
 from re import A
 
+'''
+    Aqui temos uma pequena aula sobre Tuplas, que servem para armazenar dados e trabalhar com eles.
+    vemos bem exemplificado como funcionam, sempre com parênteses () para defirnirlas. 
+
+    Uma característica muito importante das tuplas é que elas são imutáveis, ou seja, uma vez definida
+    elas não podem ser alteradas. Porém, tuplas podem ter objetos mutáveis como listas por exemplo
+
+
+
+
+'''
+# exemplo de tuplas com objetos mutáveis
+
+minha_tupla = ([1,2,3,4], ['a', 'b', 'c'])
+print(minha_tupla)
+minha_tupla[0].append(10)
+print(minha_tupla)
+
+'''
+    No exemplo acima vemos uma tupla com uma lista dentro. As listas que estão entre colchetes []
+    são mutáveis, na linha abaixo "minha_tupla[0].append(10)" eu uso o método append, para inserir um novo
+    número elemento na tupla. no caso o "10".
+
+'''
+
+
+
+
 
 lanche = ('hamburguer', 'pizza', 'pudim')
 # Tuplas são imutáveis
@@ -36,4 +64,4 @@ print(c.index(5, 1))
 
 pessoa = ('gustavo', 39, 'M', 89.9)
 del(pessoa) # podemos usar o del para apagar a tupla e não posso deletar somend 1 elemento da tupla, tenho que deletar ela por inteira.
-print(pessoa)
+print(pessoa)  # no seu programa irá retornar um erro, pois "pessoa" já não existe mais.
