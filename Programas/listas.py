5d684b2c1c49f3afd097564fa7d3c1498a1b57ec
italo <italogithub@gmail.com>
2023-01-04 20:35:32 -0300

    update

--- Programas/listas.py
+++ Programas/listas.py
@@ -17,5 +17,5 @@ valores.append(1)
 valores.append(2)
 valores.append(3)
 
-for v in valores:
-    print(f"{v}...", end="")
+for c, v in enumerate(valores):
+    print(f"Na posição {c} encontrei o valor {v}!")
