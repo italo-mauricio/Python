6b1e2020b781277cb153fb0ebf6dc6766158af7c
italo <italogithub@gmail.com>
2023-01-04 20:29:42 -0300

    update

--- Programas/listas.py
+++ Programas/listas.py
@@ -2,4 +2,20 @@ num = [1, 2, 4, 6, 5]
 num[2] = 9
 num.append(19)
 num.sort(reverse=True)
+num.insert(2, 25)
 print(num)
+if 91 in num:
+    num.remove(9)
+else:
+    print("Não achou")
+
+print(f"a minha lista possui {len(num)} elementos")
+
+
+valores = []
+valores.append(1)
+valores.append(2)
+valores.append(3)
+
+for v in valores:
+    print(f"{v}...", end="")
