0b48646228501590f8677817c6823ef8b0377759
italo <italogithub@gmail.com>
2023-01-04 21:37:13 -0300

    usando enumerate

--- Programas/exercicio_listas.py
+++ Programas/exercicio_listas.py
@@ -1,3 +1,10 @@
+'''
+        Exercicio de maior e menor valor numa lista
+
+
+'''
+
+
 num = []
 maior = 0
 menor = 0
@@ -18,3 +25,9 @@ print(f'''
     O menor valor foi {menor}
 
 ''')
+
+for i, v in enumerate(num):
+    if v == maior:
+        print(f"O maior valor apareceu posição {i}")
+    if v == menor:
+        print(f"O menor valor apareceu na posição {i}")
