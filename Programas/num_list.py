650c96f937a5145facb62d6260fa445f2b99ae3c
italo <italogithub@gmail.com>
2023-01-08 03:38:08 -0300

    update

--- Programas/num_list.py
+++ Programas/num_list.py
@@ -1,16 +1,33 @@
-num = list()
+'''
 
+        Exercicio adicionando números numa lista sem permitir repetições
+
+
+'''
+
+
+
+
+num = []
+som = 0
 while True:
     n = int(input('Digite um valor: '))
     if n not in num:
         num.append(n)
         print("Valor adicionado com sucesso!")
-        input("Pressione qualquer tecla para continuar...")
-        break   
+          
     else:
         print("Valor duplicado")
 
     r = str(input("Deseja continuar?[s/N] "))
-
     if r in 'Nn':
         break
+
+for i in num:
+    soma_tudo = i + n
+    som += soma_tudo
+
+
+
+print(f"Você adicionou os seguintes valores: {num}")
+print(f"A soma dos valores é de: {som}")
