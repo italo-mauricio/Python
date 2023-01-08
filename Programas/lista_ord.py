c1e2ab7c0e422c37f011a9f891547f4368616895
italo <italogithub@gmail.com>
2023-01-08 03:48:59 -0300

    lista ordenada

---
+++ Programas/lista_ord.py
@@ -0,0 +1,27 @@
+
+'''
+
+    Lista ordenada
+
+
+'''
+
+
+
+lista = []
+
+for i in range(0, 5):
+    num = int(input("Digite um valor: "))
+    if i == 0 or num > lista[-1]:
+        lista.append(num)
+    else:
+        pos = 0
+        while pos < len(lista):
+            if num <= lista[pos]:
+                lista.insert(pos, num)
+                break
+            pos += 1
+
+print(f"Os valores digitados em ordem foram: {lista}")
+    
+
