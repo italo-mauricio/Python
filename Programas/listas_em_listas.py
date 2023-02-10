6d3ce71fc6834c0c3563c3c03b30a16f809d10d9
italo <italogithub@gmail.com>
2023-02-10 20:25:54 -0300

    update

--- Programas/listas_em_listas.py
+++ Programas/listas_em_listas.py
@@ -5,12 +5,11 @@ Aqui vemos o conceito de listas dentro de listas
 
 '''
 
-
-
 dados = []
 dados.append('pedro')
 dados.append('25')
 pessoas = list()
 pessoas.append(dados[:])   # <---   usando esse método, podemos adicionar uma lista, dentro de outra lista
+pessoas = [['pedro, 25', ['maria, 24']], ['italo, 18']]  # posso adicionar mais elementos nessa lista, desta maneira
 
 print(pessoas)
